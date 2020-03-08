2858aa8bce48951cf4a80d78afdf34ded372c4bb
Tim Shaker <tim@tmshkr.com>
2020-03-08 15:59:33 -0700

    add dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return <div className="app">Hello World</div>;
+class App extends Component {
+  render() {
+    return <div className="app">Hello World!</div>;
+  }
 }
 
 export default App;
