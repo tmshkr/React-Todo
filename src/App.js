c80c434ccd2deba1dd239ede38ad5a647a0fbe4c
Tim Shaker <tim@tmshkr.com>
2020-03-08 16:09:23 -0700

    add localforage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
+import localforage from "localforage";
 
 class App extends Component {
+  constructor() {
+    super();
+    localforage.config();
+
+    this.state = {
+      tasks: [
+        {
+          createdAt: Date.now(),
+          title: "First task",
+          completed: false
+        }
+      ]
+    };
+  }
   render() {
     return <div className="app">Hello World!</div>;
   }
