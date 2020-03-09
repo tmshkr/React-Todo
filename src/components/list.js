336f64dd55b4d91c240ca842c2ea785bb3d6bfdc
Tim Shaker <tim@tmshkr.com>
2020-03-08 20:04:51 -0700

    task updates app state

--- src/components/list.js
+++ src/components/list.js
@@ -10,8 +10,8 @@ function List(props) {
 
   return (
     <ul className="task-list list-group">
-      {tasks.map(t => (
-        <Task key={t.id} task={t} />
+      {tasks.map((t, index) => (
+        <Task key={t.id} task={t} index={index} />
       ))}
       <CreateTask />
       <ClearCompleted />
--- src/components/task.js
+++ src/components/task.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState, memo } from "react";
+import { AppContext } from "../app";
 
 function Task(props) {
   const [title, setTitle] = useState(props.task.title);
   const [checked, setChecked] = useState(props.task.completed);
+
+  const app = useContext(AppContext);
+
+  // update app state
+  useEffect(() => {
+    const { index } = props;
+    const tasks = [...app.state.tasks];
+    const task = { ...tasks[index], title, completed: checked };
+    tasks.splice(index, 1, task);
+    app.setState({ tasks });
+  }, [title, checked]);
 
   return (
     <li className="task list-group-item">
@@ -20,4 +32,4 @@ function Task(props) {
   );
 }
 
-export default Task;
+export default memo(Task);
